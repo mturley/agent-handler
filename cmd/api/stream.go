c1727dff08256bdde02245d7b7f8efc282b36b55
Mike Turley <mike@turley.me>
2026-07-20 11:52:18 -0400

    feat: SSE stream endpoint for live UI updates
    Signed-off-by: Mike Turley <mike@turley.me>

---
+++ cmd/api/stream.go
@@ -0,0 +1,39 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func (s *Server) handleStream(w http.ResponseWriter, r *http.Request) {
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		writeError(w, http.StatusInternalServerError, "streaming not supported")
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	flusher.Flush()
+
+	ticker := time.NewTicker(3 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-r.Context().Done():
+			return
+		case <-ticker.C:
+			// Send heartbeat
+			data, err := json.Marshal(map[string]string{"type": "heartbeat"})
+			if err == nil {
+				fmt.Fprintf(w, "event: heartbeat\ndata: %s\n\n", data)
+				flusher.Flush()
+			}
+		}
+	}
+}
